5582b55a39b2f6a9f8e08c3906a5aa80f020928f
Kumari Anushka <anushka.ak14@gmail.com>
2023-01-07 22:18:43 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require("cors");
+
+const authRoutes = require("./routes/authRoutes");
+const userRoutes = require("./routes/userRoutes");
+const contactsRoutes = require("./routes/contactsRoutes");
+const matchRoutes = require("./routes/matchRoutes");
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+// middlewares
+app.use(cors({
+  origin: "*",
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+}));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/contacts", contactsRoutes)
+app.use("/api/matches", matchRoutes);
+
+mongoose.set('strictQuery', false);
+mongoose.connect(process.env.DB_URI)
+  .then(() => { app.listen(PORT, console.log("Server connected to localhost ", PORT)) })
+  .catch(err => console.log(err.message))
+
+app.get('/', (req, res) => {
+  res.send("Server running successfully...")
+})
